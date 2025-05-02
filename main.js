deab7dc9678f2e8c23292c2cc19ba65c1ef4949e
Xayot420 <asatullayevasamandar@gmail.com>
2025-05-03 00:02:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let navBar = document.querySelector('.navLinks');
+let menuBar = document.querySelector('#menuBtn');
+
+menuBar.onclick = () => {
+    navBar.classList.toggle('active');
+}
+window.onscroll = () => {
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+var swiper = new Swiper(".myHome", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  let valueDisplay = document.querySelectorAll('.num');
+  let interval = 1000;
+  valueDisplay.forEach((valueDisplay) =>{
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-value"));
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(function (){
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if(startValue == endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  });
